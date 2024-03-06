a4a4392df60d6901414d5766716b5da655ff8548
timepassuser <useless.as.shit.com@gmail.com>
2024-03-06 18:10:35 +0530

    modified api/index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
 from flask import Flask
-
+from flask import request
+from flask import make_response
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/<path:a>", methods=["GET", "OPTIONS"])
+def hello(a):
+    if request.method == "OPTIONS":
+        print("\n\n\nOPTIOnS HERE\n\n\n")
+    print('\n\nNew request here\n', request, '\n', request.headers)
+    response = make_response(f"Hi {request}  <br>  {request.headers}")
+    response.access_control_allow_origin = '*'
+    return response
+    
