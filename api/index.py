5412850a04cfedbff3337375ddee0149b32c014a
timepassuser <useless.as.shit.com@gmail.com>
2024-03-06 22:40:49 +0530

    few changes

--- api/index.py
+++ api/index.py
@@ -4,6 +4,42 @@ from flask import make_response
 import requests
 app = Flask(__name__)
 
+# @app.route("/<path:url>", methods=["GET", "OPTIONS"])
+# def hello(url):
+#     print('\n\nNew request here\n', request, '\n', request.headers)
+
+#     if request.method == "OPTIONS":
+#         response = make_response()
+#         response.access_control_allow_origin = '*'
+#         # response.access_control_allow_headers = ["corsproxy"]
+#         print(f"Sending response to options\n{response.headers}")
+#         return response
+
+#     if "https://" not in url:
+#         return "Invalid url"
+#     elif "cdn3.digialm.com" not in url:
+#         return "Invalid url"
+#     elif ".html" not in url:
+#         return "Invalid url"
+#     # elif "Corsproxy" not in request.headers.keys():
+#     #     return "Invalid request"
+#     else:
+#         # try:
+#         #     r = requests.get(url, timeout=0.5)
+#         #     if r.status_code == requests.codes.ok:
+#         #         response = make_response(r.text)
+#         #     else:
+#         #         print(f"Something went wrong, status code {r.status_code}")
+#         #         response = make_response(f"Something went wrong, status code {r.status_code}")
+#         # except Exception as exception:
+#         #     print(f"An error occured {exception}")
+#         #     response = make_response(f"An error occured {exception}")
+#         # finally:
+#         response = make_response("This is just to show it works")
+#         response.access_control_allow_origin = '*'
+#             # response.access_control_allow_headers = ["corsproxy"]
+#         return response
+
 @app.route("/<path:url>", methods=["GET", "OPTIONS"])
 def hello(url):
     print('\n\nNew request here\n', request, '\n', request.headers)
@@ -15,29 +51,29 @@ def hello(url):
         print(f"Sending response to options\n{response.headers}")
         return response
 
-    if "https://" not in url:
-        return "Invalid url"
-    elif "cdn3.digialm.com" not in url:
-        return "Invalid url"
-    elif ".html" not in url:
-        return "Invalid url"
+    # if "https://" not in url:
+    #     return "Invalid url"
+    # elif "cdn3.digialm.com" not in url:
+    #     return "Invalid url"
+    # elif ".html" not in url:
+    #     return "Invalid url"
     # elif "Corsproxy" not in request.headers.keys():
     #     return "Invalid request"
-    else:
-        # try:
-        #     r = requests.get(url, timeout=0.5)
-        #     if r.status_code == requests.codes.ok:
-        #         response = make_response(r.text)
-        #     else:
-        #         print(f"Something went wrong, status code {r.status_code}")
-        #         response = make_response(f"Something went wrong, status code {r.status_code}")
-        # except Exception as exception:
+    # else:
+    #     # try:
+    #     #     r = requests.get(url, timeout=0.5)
+    #     #     if r.status_code == requests.codes.ok:
+    #     #         response = make_response(r.text)
+    #     #     else:
+    #     #         print(f"Something went wrong, status code {r.status_code}")
+    #     #         response = make_response(f"Something went wrong, status code {r.status_code}")
+    #     # except Exception as exception:
         #     print(f"An error occured {exception}")
         #     response = make_response(f"An error occured {exception}")
         # finally:
-        response = make_response("This is just to show it works")
-        response.access_control_allow_origin = '*'
             # response.access_control_allow_headers = ["corsproxy"]
-        return response
+    response = make_response("This is just to show it works")
+    response.access_control_allow_origin = '*'
+    return response
 
     
