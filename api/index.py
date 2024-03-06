f247947a8b47632461191193b4ca7a30a399f9a5
timepassuser <useless.as.shit.com@gmail.com>
2024-03-06 20:08:24 +0530

    Modified access control allow headers

--- api/index.py
+++ api/index.py
@@ -4,12 +4,35 @@ from flask import make_response
 import requests
 app = Flask(__name__)
 
-@app.route("/<path:a>", methods=["GET", "OPTIONS"])
-def hello(a):
-    if request.method == "OPTIONS":
-        print("\n\n\nOPTIOnS HERE\n\n\n")
+@app.route("/<path:url>", methods=["GET", "OPTIONS"])
+def hello(url):
     print('\n\nNew request here\n', request, '\n', request.headers)
-    response = make_response(f"Hi {request}  <br>  {request.headers}")
-    response.access_control_allow_origin = '*'
-    return response
+
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.access_control_allow_origin = 'https://timepassuser.github.io'
+        response.access_control_allow_headers = ["corsproxy"]
+        print(f"Sending response to options\n{response.headers}")
+        return response
+
+    if "https://" not in url:
+        return "Invalid url"
+    elif "cdn3.digialm.com" not in url:
+        return "Invalid url"
+    elif ".html" not in url:
+        return "Invalid url"
+    elif "Corsproxy" not in request.headers.keys():
+        return "Invalid request"
+    else:
+        try:
+            r = requests.get(url, timeout=0.5)
+            if r.status_code == requests.codes.ok:
+                return r.text
+            else:
+                print(f"Something went wrong, status code {r.status_code}")
+                return f"Something went wrong, status code {r.status_code}"
+        except Exception as exception:
+            print(f"An error occured {exception}")
+            return f"An error occured {exception}"
+
     
